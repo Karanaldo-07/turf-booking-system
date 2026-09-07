587487617bcb8ed7278b0c94f545440991761d0a
Karanaldo-07 <karanbhise126@gmail.com>
2026-09-07 11:34:32 +0530

    Validate turf data

--- server/src/models/Turf.js
+++ server/src/models/Turf.js
@@ -2,18 +2,25 @@ const mongoose = require('mongoose');
 
 const turfSchema = new mongoose.Schema(
   {
-    name: { type: String, required: true },
-    location: { type: String, required: true },
-    description: { type: String },
-    image: { type: String },
-    basePricePerHour: { type: Number, required: true },
+    name: { type: String, required: true, trim: true, minlength: 2, maxlength: 100 },
+    location: { type: String, required: true, trim: true, maxlength: 200 },
+    description: { type: String, trim: true, maxlength: 1000 },
+    image: { type: String, trim: true, maxlength: 1000 },
+    basePricePerHour: { type: Number, required: true, min: 1 },
     isActive: { type: Boolean, default: true },
     availableHours: {
-      start: { type: Number, default: 6 },
-      end: { type: Number, default: 23 }
+      start: { type: Number, min: 0, max: 23, default: 6 },
+      end: { type: Number, min: 1, max: 24, default: 23 }
     }
   },
   { timestamps: true }
 );
+
+turfSchema.pre('validate', function validateHours(next) {
+  if (this.availableHours.start >= this.availableHours.end) {
+    return next(new Error('Turf opening time must be before closing time.'));
+  }
+  return next();
+});
 
 module.exports = mongoose.model('Turf', turfSchema);
