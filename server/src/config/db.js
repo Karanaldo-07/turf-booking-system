9cac8ac66332ac0d3b85ed0f46397a97ef5415f6
Karanaldo-07 <karanbhise126@gmail.com>
2026-09-07 12:54:03 +0530

    Fix MongoDB environment variable for Vercel

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -5,12 +5,14 @@ let connectionPromise;
 const connectDb = async () => {
   if (mongoose.connection.readyState === 1) return mongoose.connection;
 
-  if (!process.env.MONGO_URI) {
-    throw new Error('MONGO_URI is not configured');
+  const mongoUri = process.env.MONGODB_URI || process.env.MONGO_URI;
+
+  if (!mongoUri) {
+    throw new Error('MONGODB_URI is not configured');
   }
 
   if (!connectionPromise) {
-    connectionPromise = mongoose.connect(process.env.MONGO_URI, {
+    connectionPromise = mongoose.connect(mongoUri, {
       serverSelectionTimeoutMS: 10000
     }).then(() => {
       console.log('MongoDB connected');
