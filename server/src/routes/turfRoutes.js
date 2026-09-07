5bfe1ad6e3794425fc4f5098f8e1a51402325125
Karanaldo-07 <karanbhise126@gmail.com>
2026-09-07 11:35:03 +0530

    Wrap turf controllers for async errors

--- server/src/routes/turfRoutes.js
+++ server/src/routes/turfRoutes.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const { getTurfs, adminGetTurfs, createTurf, updateTurf, deleteTurf } = require('../controllers/turfController');
 const { protect, adminOnly } = require('../middleware/authMiddleware');
+const asyncHandler = require('../utils/asyncHandler');
 
 const router = express.Router();
 
-router.get('/', getTurfs);
-router.get('/admin/all', protect, adminOnly, adminGetTurfs);
-router.post('/', protect, adminOnly, createTurf);
-router.put('/:id', protect, adminOnly, updateTurf);
-router.delete('/:id', protect, adminOnly, deleteTurf);
+router.get('/', asyncHandler(getTurfs));
+router.get('/admin/all', protect, adminOnly, asyncHandler(adminGetTurfs));
+router.post('/', protect, adminOnly, asyncHandler(createTurf));
+router.put('/:id', protect, adminOnly, asyncHandler(updateTurf));
+router.delete('/:id', protect, adminOnly, asyncHandler(deleteTurf));
 
 module.exports = router;
