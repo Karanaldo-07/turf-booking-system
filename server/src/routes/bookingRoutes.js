14565d20ebf5c77641bafaf5ab6cea8fa12f52a1
Karanaldo-07 <karanbhise126@gmail.com>
2026-09-07 11:34:58 +0530

    Wrap booking controllers for async errors

--- server/src/routes/bookingRoutes.js
+++ server/src/routes/bookingRoutes.js
@@ -8,14 +8,15 @@ const {
   updateBookingStatus
 } = require('../controllers/bookingController');
 const { protect, adminOnly } = require('../middleware/authMiddleware');
+const asyncHandler = require('../utils/asyncHandler');
 
 const router = express.Router();
 
-router.get('/me', protect, getMyBookings);
-router.get('/admin/all', protect, adminOnly, getAllBookings);
-router.post('/', protect, createBooking);
-router.post('/confirm-payment', protect, confirmPayment);
-router.delete('/:id', protect, cancelMyBooking);
-router.put('/admin/:id/status', protect, adminOnly, updateBookingStatus);
+router.get('/me', protect, asyncHandler(getMyBookings));
+router.get('/admin/all', protect, adminOnly, asyncHandler(getAllBookings));
+router.post('/', protect, asyncHandler(createBooking));
+router.post('/confirm-payment', protect, asyncHandler(confirmPayment));
+router.delete('/:id', protect, asyncHandler(cancelMyBooking));
+router.put('/admin/:id/status', protect, adminOnly, asyncHandler(updateBookingStatus));
 
 module.exports = router;
